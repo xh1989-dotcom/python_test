1eeb588f3a0e5b834cf5f4e2328110dc4cbd64bb
xuhang <xuhang1128@gmail.com>
2025-12-20 22:15:41 +0800

    ignore

--- flask_api_example.py
+++ flask_api_example.py
@@ -4,14 +4,21 @@ Flask API示例
 功能：创建一个简单的HTTP接口返回Hello World
 """
 
+import os
 from flask import Flask, jsonify
+from dotenv import load_dotenv
+
+# 加载.env文件中的环境变量
+load_dotenv()
 
 app = Flask(__name__)
 
+# 从环境变量获取name，默认值为'World'
+name = os.environ.get('name', 'World')
+
 @app.route('/hello', methods=['GET'])
 def hello():
-    print("Hello World")
-    return jsonify({'message': 'Hello World'})
+    return jsonify({'message': f'Hello {name}'})
 
 @app.route('/', methods=['GET'])
 def index():
